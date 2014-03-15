35b7951b23b7b15ee5397998de671af2dd60fa67
Zachary Bush <Zachary.bush@meraki.net>
2014-03-15 12:50:39 -0700

    clean up yelp display. Use REact

--- src/js/doctor.js
+++ src/js/doctor.js
@@ -1,6 +1,10 @@
+/** @jsx React.DOM */
+
 function yelpHandlerFor(dn) {
   return function(data) {
-    n = dn.find('.yelp');
+    n = dn.find('.yelp')[0];
+    React.renderComponent(<YelpComponent businesses={data.businesses} />, n);
+    /* 
     for (i = 0; i < data.businesses.length; i++) {
       var b = data.businesses[i];
       n.append('<div class="node' + i + '"></div>');
@@ -15,25 +19,32 @@ function yelpHandlerFor(dn) {
     if (stuff != "()") {
       n.append(stuff);
     }
+    */
   };
 }
 
-
 function main() {
-  var providers = $('.providerContainer');
+  var base_providers = $('.providerContainer');
+  var iframe_providers = $('iframe').contents().find('.providerContainer');
+  var providers = $.merge(base_providers, iframe_providers);
   for (i = 0; i < providers.length; i++) {
     var p = $(providers[i]);
     if (p.data('_doctoring_applied_')) continue;
     var doctor_node = $('<div class="doctor"></div>');
+    doctor_node.css({
+      "clear": "both",
+      "padding-left": "10px",
+      "margin-right": "10px",
+      "margin-bottom": "5px"
+    });
     doctor_node.append('<div class="yelp"></div>');
     p.data('_doctoring_applied_', doctor_node);
-    p.append(doctor_node);
+    p.find('.providerBox').append(doctor_node);
 
     // The below will only apply to a providerContainer once
-    $.get('//api.yelp.com/business_review_search', {
+    $.get('//api.yelp.com/phone_search', {
       ywsid: '_Q514U0COwKR-CLO80mYfA',
-      term: p.find('.providerName').text(),
-      location: p.find('.address').text(),
+      phone: p.find('.phone').text()
     }, yelpHandlerFor(doctor_node));
     /*
       url: http://api.yelp.com/business_review_search?term=yelp&tl_lat=37.9&tl_long=-122.5&br_lat=37.788022&br_long=-122.399797&limit=3&ywsid=_Q514U0COwKR-CLO80mYfA
--- src/js/yelp.js
+++ src/js/yelp.js
@@ -1,8 +1,72 @@
 /** @jsx React.DOM */
 
-YelpComponent = React.createClass({
+YelpBusiness = React.createClass({
+  visit: function() {
+    window.open(this.props.b.url, '_blank');
+  },
   render: function() {
-    return <div>
+    var b = this.props.b;
+    var previous_cat = false;
+    var business_style = {
+      "margin-top": "5px",
+      "border": "1px solid black",
+      "border-radius": "10px",
+      "padding": "5px",
+      "height": "100px"
+    };
+
+    var img_style = {
+      "float": "left"
+    };
+
+    var info_style = {
+      "float": "right",
+      "width": "80%"
+    };
+
+    return <div className="business"
+                onClick={ this.visit }
+                style={ business_style }>
+      <div className="img"
+           style={img_style}>
+        <img src={ b.photo_url } />
+      </div>
+      <div className="information"
+           style={info_style}>
+        <div className="address">
+          <b>{ b.name }</b> (<img src={ b.rating_img_url_small } /> / { b.review_count } )<br />
+          {
+            b.categories.map(function(cat) {
+              if (previous_cat) {
+                return <span>/ {cat.name}</span>;
+              } else {
+                previous_cat = true;
+                return <span>{cat.name}</span>;
+              }
+            })
+          }<br />
+          { b.address1 } { b.address2 } { b.address3 }
+          { b.city }, { b.state } { b.zip }<br />
+        </div>
+      </div>
+    </div>;
+  }
+});
+YelpComponent = React.createClass({
+  getDefaultProps: function() {
+    return {
+      businesses: []
+    };
+  },
+
+  render: function() {
+    return <div className="yelp">
+      {
+        this.props.businesses.map(function(b) {
+          return <YelpBusiness b={ b }
+                               key={ "business" + b.id }/>;
+        })
+      }
     </div>;
   }
 });
